2fee1df5f08cf947b2c73f891238bc8309bcddd6
Nicolas Firavitoba <nicofira.dev@gmail.com>
2019-03-21 17:54:26 -0500

    Delete home.js content

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,1 @@
-// $.ajax({
-//   method: 'get',
-//   url: 'https://api.iextrading.com/1.0/stock/market/list/mostactive'
-// }).done((data) => {
-//   console.log(data)
-//   data.forEach(() => {
 
-//   });
-// });
